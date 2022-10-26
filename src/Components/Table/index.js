b1b6a444188e76d322302626240dfab6de11f6ea
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2022-10-25 21:07:55 -0300

    Add files via upload

---
+++ src/Components/Table/index.js
@@ -0,0 +1,27 @@
+import { TableRow } from './TableRow';
+import './table.css';
+
+export function Table({ tasks, removeTask }) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Descrição da Tarefa</th>
+                    <th>Data de Início</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    tasks.map(task => (
+                        <TableRow
+                            key={task.id}
+                            task={task}
+                            removeTask={removeTask} />
+                    ))
+
+                }
+            </tbody>
+        </table>
+    )
+}
---
+++ src/Components/TaskCounter.js
@@ -0,0 +1,7 @@
+export function TaskCounter({ tasks }) {
+    return (
+        <span class="total-tasks">
+            <strong>{tasks}</strong> tarefas registradas.
+        </span>
+    )
+}
